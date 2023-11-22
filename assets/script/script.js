3ad8a1a711c37559ccba92e00b288457423d790d
Elena <Eva71879@gmail.com>
2023-11-22 22:45:04 +0300

    added checked block

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,19 @@
 const input = document.querySelector('.input');
 const taskList = document.querySelector('.taskList');
 function postTask () {
-    const newTask = document.createElement('li');
-    newTask.innerHTML = input.value;
-    taskList.append(newTask);
+    if (input.value.trim().length) {
+        const newTask = document.createElement('li');
+        newTask.innerHTML = input.value;
+        taskList.append(newTask);
+        input.value = ""; // Здесь обнуляем значение инпута
+       }
 }
+
+function checkTask(evt) {
+    if (evt.target.tagName === "LI") {
+     evt.target.classList.toggle("checked");
+    }
+   }
+taskList.onclick = checkTask;
 
 document.querySelector('.button').onclick = postTask;
